f542d8ce617567abb7872bcec80e748a3dbe1d06
omurayuki <g1221438@icloud.com>
2019-09-04 11:28:16 +0900

    gitignore修正

--- UITest/UITest.swift
+++ UITest/UITest.swift
@@ -11,15 +11,12 @@ import XCTest
 class UITest: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        //        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        // snapshot("好きな名前") snapshotで撮影, ""が写真の名前になる
+        app.launch()
     }
 
     override func tearDown() {
