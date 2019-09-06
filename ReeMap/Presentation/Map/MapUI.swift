02d381a6ff8e8b5da6dc609c27010c1fb787df55
omurayuki <g1221438@icloud.com>
2019-09-06 12:12:23 +0900

    UIにMap配置

--- ReeMap/Presentation/Map/MapUI.swift
+++ ReeMap/Presentation/Map/MapUI.swift
@@ -1,13 +1,26 @@
 import Foundation
 import UIKit
+import MapKit
+import CoreLocation
 
 protocol MapUIProtocol: UI {
-    
+    var mapView: MKMapView { get }
 }
 
 final class MapUI: MapUIProtocol {
     
     weak var rootView: UIView?
+    
+    private(set) var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        return mapView
+    }()
+    
+    private var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        return locationManager
+    }()
 }
 
 extension MapUI {
